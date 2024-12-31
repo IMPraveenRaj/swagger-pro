bd597e1a48c0bc33590f25f53dbd9bfc4d1978d2
IMPraveenRaj <impraveen13@gmail.com>
2024-12-31 22:05:54 +0100

    initial commit for swagger pro series

---
+++ src/test/java/com/springheaven/swagger_demo/SwaggerProApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springheaven.swagger_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwaggerProApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
